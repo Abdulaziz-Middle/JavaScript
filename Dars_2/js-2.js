cc74240c5d9194ad0cea29899976b76fd18a4a2a
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-24 13:51:52 +0500

    Add files via upload

---
+++ Dars_2/js-2.js
@@ -0,0 +1,71 @@
+//let age = 15; //data-type = number
+
+//let myName = "Abdulaziz"; //data-type = string
+
+//let boolean = true || false; //data-type = boolean
+
+//let undefined; //data-type = undefined
+
+// console.log(a); //data-type = null
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
+
+// object
+
+// let object = {
+//     name: "Abdulaziz", // string
+//     age: 15, // number
+//     like: false, //boolean
+// };
+// console.log(object);
+// // array
+
+// let arr = ['Phone','Mouse','Soap'];
+// console.log(arr);
+// // console.log(arr[0]);
+// arr[0] = "Msi";
+// console.log(arr);
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
+
+let x = 5;
+let y = 6;
+// console.log(x+5);
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+let z = 15;
+console.log(z%7);
+
+// x = x + 1;
+// x += 1;
+x ++;
+console.log(x);
+
+// x = x - 1;
+// x -= 1;
+x --;
+console.log(x);
+
+let a = 10;
+let b = 10;
+// console.log(10 == 10);
+console.log(a == b);
+console.log('10' == 10); // true
+console.log('10' === 10); // false
+
+console.log(4/0); // Cheksizlik yoki Infinity
+console.log(-4/0); // -Cheksizlik yoki -Infinity
+
+console.log("4" + 4); // 44
+
+// alert("Hello world");
+// confirm("sizni yoshiz 15mi"); savol xa yoki yoq
+// let abc = confirm("sizni yoshiz 15mi");
+// console.log(abc);
+
+// prompt("What is your name ?");
+let abc = prompt("What is your name ?");
+console.log(abc);
