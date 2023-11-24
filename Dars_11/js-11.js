dadd62ddbd8743f5dd7ab0326de899ca596c54d4
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-24 13:54:42 +0500

    Add files via upload

---
+++ Dars_11/js-11.js
@@ -0,0 +1,19 @@
+// const car = {
+//     name: "BMW",
+//     color: "black",
+//     extraBalon: 5,
+//     speed: function () {
+//         console.log("100 km/h");
+//     }
+// }
+
+// Object.prototype.seyHello = function (){
+//     console.log("Hello World");
+// }
+
+// const bus = Object.create(car);
+// bus.name = "Abdulazizbek";
+// bus.extraBalon = 10;
+
+// str = "Hello World String Data Type"
+// const str = new String("Hello World String Data Type");
