0915776ecfe1dd1b8ee2026d2ef5b11efae74375
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-24 13:52:21 +0500

    Add files via upload

---
+++ Dars_3/js-3.js
@@ -0,0 +1,33 @@
+// 1-Ko'rinishi
+// for (let i = 1; i<=10;i++){
+//     console.log(i);
+// }
+
+// 2-Ko'rinishi
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (
+//     i < 10
+// )
+
+// 3-Ko'rinishi
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
+
+// let answers = [];
+// let questions = [
+//     'What is your name ?',
+//     'What is your phone ?',
+//     'How old are you',
+// ];
+// for (let i = 0; i<questions.length;i++){
+//     answers[i] = prompt(questions[i])
+// }
+// console.log(answers);
