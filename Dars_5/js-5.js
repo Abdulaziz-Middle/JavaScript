2d57b386c05827133488673bb6b8194ad3a856ef
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-24 13:53:03 +0500

    Add files via upload

---
+++ Dars_5/js-5.js
@@ -0,0 +1,55 @@
+// // // Function declaration
+// // let num = 5;
+// // function myFirstFunction(argument){
+// //     // console.log("Hi my name is Abdulaziz");
+// //     console.log(`Hi my name ${argument}`);
+// //     let num = 10;
+// //     num++;
+// //     console.log(num);
+// // }
+// // myFirstFunction("Abdulaziz");
+// // console.log(num);
+// // // Function express
+// // // let myFunction = function(){
+// // //     console.log("This Function is express");
+// // // }
+// // // myFunction()
+
+// // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
+
+// function myFirstAPp(name, age) {
+//     // alert(`Hi my name is ${name} and i am full-stact developer and age ${age}`)
+//     // function showSkills() {
+//     //     let skills = ["Html", "Css", "JavaScript"];
+//     //     for (let i = 0; i < skills.length; i = i + 1) {
+//     //         alert(`You have skills ${skills[i]}`)
+//     //     }
+//     // }
+//     // showSkills()
+//     function chekAge() {
+//         //age > 18 Exactly data for learning IT
+//         //age < 18 Super data for learning IT
+//         let age = +prompt("Yoshingiz nechida");
+//         if (age > 18) {
+//             alert("Siz super data");
+//         } else if (age < 18) {
+//             alert("Siz Exactly data");
+//         }
+//         else {
+//             alert("Sizni yoshingiz nomalum")
+//         }
+//     }
+//     chekAge()
+//     // function calcPow(num) {
+//     //     return num * num
+//     // }
+//     // console.log(calcPow(5));
+// }
+// myFirstAPp("Abdulaziz", 15)
+
+// // function name(){
+// //     return "Hi my name is Abdulaziz";
+// // }
+// // console.log(name());
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
