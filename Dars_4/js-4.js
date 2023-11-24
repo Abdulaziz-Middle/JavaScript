4f90cafb98c9676aa54cd4b46072569cb19a9a68
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-24 13:52:42 +0500

    Add files via upload

---
+++ Dars_4/js-4.js
@@ -0,0 +1,27 @@
+// let age = prompt("Yoshingiz nechida");
+// if(age<18){
+//     alert("Hali Vohli")
+// }else if (age>80){
+// alert("Uje kech")
+// }else{
+//     alert("Hush kelib siz")
+// }
+
+// 1-Ko'rinishi
+// let age = prompt("Yoshingiz nechida");
+// switch (age){
+//     case 18:alert("Sizni yoshingiz 18da")
+//     break;
+//     case 50:alert("Sizni yoshingiz 50da")
+//     break;
+//     default:alert("Sizni yoshingiz nomalum")
+// }
+// 2-Ko'rinishi
+// let age = +prompt("Yoshingiz nechida");
+// switch (age){
+//     case '18':alert("Sizni yoshingiz 18da")
+//     break;
+//     case '50':alert("Sizni yoshingiz 50da")
+//     break;
+//     default:alert("Sizni yoshingiz nomalum")
+// }
