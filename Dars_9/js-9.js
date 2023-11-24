9f3aee0faa527fc23396cea527d357941bd56aa7
Abdulaziz <151828455+Abdulaziz-Middle@users.noreply.github.com>
2023-11-24 13:54:22 +0500

    Add files via upload

---
+++ Dars_9/js-9.js
@@ -0,0 +1,38 @@
+// const number = 8;
+// localStorage.removeItem("number");
+// console.log(localStorage.getItem("number"));
+// localStorage.setItem("number",number.toString())
+// console.log(localStorage.getItem('number'));
+// localStorage.clear()
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
+
+// const persone = {
+//     name:"Abdulaziz",
+//     age:15
+// }
+// localStorage.setItem('persone',persone);
+// const json = JSON.stringify(persone);
+// console.log("objectdan jsonga o'tyapti",json);
+// const obj = JSON.parse(json);
+// console.log("JSONdan objectga o'tyapti",obj);
+
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = \\
+
+// const persone = {
+//     name:"Abdulaziz",
+//     age:15
+// }
+
+// localStorage.setItem('persone',JSON.stringify(persone));
+
+// const LocalData = localStorage.getItem("persone");
+// // console.log(LocalData);
+// // console.log(JSON.parse(LocalData));
+
+// const PersoneInfo = JSON.parse(LocalData);
+// PersoneInfo.name ="Azizbek_Middle_Uzb";
+// console.log(PersoneInfo);
+// localStorage.setItem("UpdatePersoneData",JSON.stringify(PersoneInfo))
+
+// // bunda local storegedagi malumot olib o'zgartirib qoyildi
